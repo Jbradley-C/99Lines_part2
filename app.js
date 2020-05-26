f4a8c752982600e8d2e9b9b591499d0ff4bad535
Jbradley-C <jbradley.chapman@gmail.com>
2020-05-26 10:32:04 -0500

    added the divs and song lyrics for each friend'

--- app.js
+++ app.js
@@ -11,42 +11,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	button.addEventListener('click', function() {
 
-
 		let Friends = [ 'John', 'Jacob', 'Jingle', 'Himer', 'Schmitt' ];
 		//Loops for the songs
 		for (let i = 0; i < Friends.length; i++) {
-			console.log(Friends[i + ':']);
+
+			let friendDiv = document.createElement("div");
+			friendDiv.classList.add("friend");
+			let friendH3 =document.createElement("h3");
+			friendH3.textContent = Friends[i];
+			friendDiv.appendChild(friendH3);
+			document.body.appendChild(friendDiv);
 
 			for (let num = 99; num > 0; num--) {
+
+				let newLyricLine = document.createElement("p");
+				friendDiv.appendChild(newLyricLine);
+
+
 				if (num === 2) {
-					console.log(
-						num +
-							' Lines of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + 				(num - 1) +
-							' Line of code in the file!'
-					);
+				
+					newLyricLine.textContent =  num + ' Lines of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + (num - 1) + ' Line of code in the file!';
+					;
+
 				} else if (num === 1) {
-					console.log(
-						num +
-							' Line of code in the file! ' +
-							num +
-							' Lines of Code! ' +
-							Friends[i] +
-							' deletes one line, clears it all out.' +
-							'.. ' +
-							' no more lines of code in the file!'
-					);
+					newLyricLine.textContent = num  + ' Line of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + ' no more lines of code in the file!';
+
 				} else {
-					console.log(
-						num +
-							'  Lines of code in the file! ' +
-							num +
-							' Line of Code! ' +
-							Friends[i] +
-							' deletes one line, clears it all out.' +
-							'.. ' +
-							(num - 1) +
-							'  lines of code in the file!'
-					);
+
+					newLyricLine.textContent = num  + '  Lines of code in the file! ' + num + ' Line of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + (num - 1) + '  lines of code in the file!';
 				}
 			}
 		}
