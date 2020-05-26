d13cc872d790145da21a15ab56fd070ace474ade
Jbradley-C <jbradley.chapman@gmail.com>
2020-05-26 10:42:36 -0500

    formatted a little and added comments

--- app.js
+++ app.js
@@ -12,32 +12,37 @@ document.addEventListener('DOMContentLoaded', function() {
 	button.addEventListener('click', function() {
 
 		let Friends = [ 'John', 'Jacob', 'Jingle', 'Himer', 'Schmitt' ];
-		//Loops for the songs
-		for (let i = 0; i < Friends.length; i++) {
 
+		//Looping through each friend
+		for (let i = 0; i < Friends.length; i++) {
+		
+		//Add a div with class of friend for each loop of the array	
 			let friendDiv = document.createElement("div");
 			friendDiv.classList.add("friend");
-			let friendH3 =document.createElement("h3");
+		
+		//Add a header with the name of the current friend inside the header	
+			let friendH3 = document.createElement("h3");
 			friendH3.textContent = Friends[i];
 			friendDiv.appendChild(friendH3);
 			document.body.appendChild(friendDiv);
 
+		//Looping through the lyrics of the song	
 			for (let num = 99; num > 0; num--) {
 
+        //Adding a paragraph with all 99 lines in it for each friend's header
 				let newLyricLine = document.createElement("p");
 				friendDiv.appendChild(newLyricLine);
 
-
-				if (num === 2) {
-				
+        //If number of lines in is 2, change the grammer to match and put it in the paragraph
+				if (num === 2) {				
 					newLyricLine.textContent =  num + ' Lines of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + (num - 1) + ' Line of code in the file!';
-					;
-
+					
+        //If the number of lines is 1, change grammer to match and put it in the paragraph 
 				} else if (num === 1) {
 					newLyricLine.textContent = num  + ' Line of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + ' no more lines of code in the file!';
 
+        //All other lines of the song and add it to paragraph					
 				} else {
-
 					newLyricLine.textContent = num  + '  Lines of code in the file! ' + num + ' Line of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + (num - 1) + '  lines of code in the file!';
 				}
 			}
