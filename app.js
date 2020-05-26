61d3e6fad78beef672490b5856f6d0f79ffcb1ac
Jbradley-C <jbradley.chapman@gmail.com>
2020-05-26 09:46:54 -0500

    added button to console log song and deleted divs containing friends

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
-let Friends = [ 'John', 'Jacob', 'Jingle', 'Himer', 'Schmitt' ];
+document.addEventListener('DOMContentLoaded', function() {
+	//Creating the button and Text for the button
+	let button = document.createElement('button');
+	let btnText = document.createTextNode('Sing');
 
-//Loops for the songs
-for (let i = 0; i < Friends.length; i++) {
-	console.log(Friends[i + ':']);
+	//Adding Text to the Button
+	button.appendChild(btnText);
 
-	for (let num = 99; num > 0; num--) {
-		if (num === 2) {
-			console.log(
-				num + ' Lines of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' +	(num - 1) +  ' Line of code in the file!'
-			);
+	//Adding button to the body of HTML
+	document.body.appendChild(button);
 
-		} else if (num === 1) {
-			console.log(
-				num + ' Line of code in the file! ' +	num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + ' no more lines of code in the file!' 
-			);
+	button.addEventListener('click', function() {
 
-		} else {
-			console.log(num + '  Lines of code in the file! ' + num + ' Line of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' +  (num - 1) + '  lines of code in the file!'
-			);
+
+		let Friends = [ 'John', 'Jacob', 'Jingle', 'Himer', 'Schmitt' ];
+		//Loops for the songs
+		for (let i = 0; i < Friends.length; i++) {
+			console.log(Friends[i + ':']);
+
+			for (let num = 99; num > 0; num--) {
+				if (num === 2) {
+					console.log(
+						num +
+							' Lines of code in the file! ' + num + ' Lines of Code! ' + Friends[i] + ' deletes one line, clears it all out.' + '.. ' + 				(num - 1) +
+							' Line of code in the file!'
+					);
+				} else if (num === 1) {
+					console.log(
+						num +
+							' Line of code in the file! ' +
+							num +
+							' Lines of Code! ' +
+							Friends[i] +
+							' deletes one line, clears it all out.' +
+							'.. ' +
+							' no more lines of code in the file!'
+					);
+				} else {
+					console.log(
+						num +
+							'  Lines of code in the file! ' +
+							num +
+							' Line of Code! ' +
+							Friends[i] +
+							' deletes one line, clears it all out.' +
+							'.. ' +
+							(num - 1) +
+							'  lines of code in the file!'
+					);
+				}
+			}
 		}
-	}
-}
+	});
+});
